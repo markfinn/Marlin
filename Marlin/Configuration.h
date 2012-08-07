9416649b2c77885a5cacbcfddbaaf3fe2442394d
Mark Finn <mfinn@fedex.com>
2012-08-07 09:57:23 -0500

    starting the config for mendelmax

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1,9 +1,6 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
-//temp while starting up mendelmax
-#define BOGUS_TEMPERATURE_FAILSAFE_OVERRIDE
-
 // This configurtion file contains the basic settings.
 // Advanced settings can be found in Configuration_adv.h 
 // BASIC SETTINGS: select your board type, temperature sensor type, axis scaling, and endstop configuration
@@ -108,12 +105,15 @@
 //    #define  DEFAULT_Ki 1.08  
 //    #define  DEFAULT_Kd 114  
 
-//do this
 // Makergear - mark from pidautotune
-    #define  DEFAULT_Kp 45.08
-    #define  DEFAULT_Ki 2.59
-    #define  DEFAULT_Kd 196.46
+//    #define  DEFAULT_Kp 45.08
+//    #define  DEFAULT_Ki 2.59
+//    #define  DEFAULT_Kd 196.46
 
+// medelmax - mark from pidautotune
+    #define  DEFAULT_Kp 34.89
+    #define  DEFAULT_Ki 3.55
+    #define  DEFAULT_Kd 80
 
 // Mendel Parts V9 on 12V    
 //    #define  DEFAULT_Kp  63.0
@@ -194,10 +194,10 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 #define max_software_endstops true  //If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 200
-#define X_MIN_POS 0
-#define Y_MAX_POS 198
-#define Y_MIN_POS 0
+#define X_MAX_POS (257-20)
+#define X_MIN_POS 20
+#define Y_MAX_POS (347-60)
+#define Y_MIN_POS 60
 #define Z_MAX_POS 86
 #define Z_MIN_POS 0
 
@@ -218,7 +218,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 
 //your retract speed in slic3r should be set to 10mm/sec.
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 3200/(25.4/12), 826}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 3200/(25.4/12), 885}
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 15} // (mm/sec) 
 #define DEFAULT_MAX_ACCELERATION      {900,900,80,500} // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
