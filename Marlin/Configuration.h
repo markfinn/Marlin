53f2c7cc80e23a33e7dba5c8354a230af2834456
Mark Finn <mark@mfinn.net>
2015-01-17 23:51:57 -0600

    reduce overshoot by not enabling PID until near temp

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -150,7 +150,7 @@
   //#define PID_DEBUG // Sends debug data to the serial port.
   //#define PID_OPENLOOP 1 // Puts PID in open loop. M104/M140 sets the output power from 0 to PID_MAX
   //#define SLOW_PWM_HEATERS // PWM with very low frequency (roughly 0.125Hz=8s) and minimum state time of approximately 1s useful for heaters driven by a relay
-  #define PID_FUNCTIONAL_RANGE 100 // If the temperature difference between the target temperature and the actual temperature
+  #define PID_FUNCTIONAL_RANGE 30 // If the temperature difference between the target temperature and the actual temperature
                                   // is more then PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
   #define PID_INTEGRAL_DRIVE_MAX 135  //limit for the integral term
   #define K1 0.95 //smoothing factor withing the PID
